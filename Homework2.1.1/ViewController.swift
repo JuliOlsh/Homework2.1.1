c77abc5ca33c43b2bb843a4c18f656d408a37bd4
Juli Olsh <juli.olsh@icloud.com>
2023-05-19 12:18:12 +0200

    Initial Commit

---
+++ Homework2.1.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework2.1.1
+//
+//  Created by Юлия on 05.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Homework2.1.2.swift
@@ -0,0 +1,8 @@
+//
+//  Homework2.1.2.swift
+//  Homework2.1.1
+//
+//  Created by Юлия on 09.05.2023.
+//
+
+import Foundation
